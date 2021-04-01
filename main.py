6fdb62afa5b7e96c85554d885f73825c8dfd5787
gmongi <guillox182@gmail.com>
2021-04-01 14:08:21 -0300

    Add parameterized snscrape command

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+
+flags_placeholder = "[FLAGS]"
+file_name = "laplatamlp-tweets"
+base_command = f"snscrape {flags_placeholder} > {file_name}.json"
+since = "2020-11-01"
+until = "2020-11-05"
+max_results = 1
+flags = {
+    "--jsonl": None,
+    "--progress": None,
+    "--max-results": max_results,
+    "--since": since,
+    "twitter-search": '"(#Coronavirus) (from:LaPlataMLP) -filter:replies '
+                      f'until:{until}"',
+
+}
+
+# Build command string
+parsed_flags = [f"{key} {value or ''} " for key, value in flags.items()]
+command = base_command.replace(flags_placeholder, "".join(parsed_flags))
+# Call snscrape via OS library
+os.system(command)
