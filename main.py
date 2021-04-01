9cb67fda3580bc55d8342739a9e8eeb32762bcad
gmongi <guillox182@gmail.com>
2021-04-01 16:27:20 -0300

    Move Since to config.ini & Remove Until

--- main.py
+++ main.py
@@ -8,16 +8,13 @@ tw_account = config['DEFAULT']['TwitterAccount']
 
 os.makedirs(directory, exist_ok=True)
 file_name = f"{directory}/{tw_account}-tweets"
-since = "2020-11-01"
-until = "2020-11-05"
 max_results = 1
 flags = {
     '--jsonl': None,
     '--progress': None,
     '--max-results': max_results,
-    '--since': since,
-    'twitter-search': f'"(#Coronavirus) (from:{tw_account}) -filter:replies '
-                      f'until:{until}"',
+    '--since': config['DEFAULT']['DateSince'],
+    'twitter-search': f'"(#Coronavirus) (from:{tw_account}) -filter:replies"',
 }
 
 # Build command string
