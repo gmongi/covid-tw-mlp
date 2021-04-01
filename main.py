d42033311d9a4f1b4cf14bae9089bc8e90241886
gmongi <guillox182@gmail.com>
2021-04-01 17:15:58 -0300

    Parse JSON tweets after scrapping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import configparser
+import json
 import os
 
 # Config setup
@@ -11,7 +12,7 @@ date_since = config['DEFAULT']['DateSince']
 
 # Files variables
 os.makedirs(directory, exist_ok=True)
-file_name = f"{directory}/{tw_account}-tweets"
+file_name = f"{directory}/{tw_account}-tweets.json"
 
 # Command flags
 flags = {
@@ -24,7 +25,17 @@ flags = {
 
 # Build command string
 parsed_flags = [f"{key} {value or ''} " for key, value in flags.items()]
-command = f"snscrape {''.join(parsed_flags)} > {file_name}.json"
+command = f"snscrape {''.join(parsed_flags)} > {file_name}"
 
 # Call snscrape via OS library
 os.system(command)
+
+# Read the json file we just scraped
+json_tweets = []
+with open(file_name) as f:
+    for line in f:
+        tweet = json.loads(line)
+        json_tweets.append(tweet)
+
+for tweet in json_tweets:
+    print(tweet['content'])
