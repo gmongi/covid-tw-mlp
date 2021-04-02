1df9a178b223bbee830dd9faaaaf5687d8e59699
gmongi <guillox182@gmail.com>
2021-04-01 21:14:57 -0300

    Download previewImage from each scrapped tweet

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import json
 import os
 from datetime import date
 
+import requests
+
 # Config setup
 config = configparser.ConfigParser()
 config.read('config.ini')
-directory = config['DEFAULT']['ScrappedTweeetsDir']
+tw_dir = config['DEFAULT']['ScrappedTweeetsDir']
 tw_account = config['DEFAULT']['TwitterAccount']
+img_dir = config['DEFAULT']['ImageDownloadDir']
 max_results = config['DEFAULT']['MaxResults']
 last_day_path = 'lastScrapped.tmp'
 if os.path.exists(last_day_path):
@@ -16,9 +19,12 @@ if os.path.exists(last_day_path):
 else:
     date_since = config['DEFAULT']['DateSince']
 
+# Images directory
+os.makedirs(img_dir, exist_ok=True)
+
 # Tweets directory and json file
-os.makedirs(directory, exist_ok=True)
-file_name = f"{directory}/{tw_account}-tweets.json"
+os.makedirs(tw_dir, exist_ok=True)
+file_name = f"{tw_dir}/{tw_account}-tweets.json"
 
 # Command flags
 flags = {
@@ -44,8 +50,12 @@ with open(file_name) as f:
         json_tweets.append(tweet)
 
 # Work with the tweets
+# Download images to img_dir
 for tweet in json_tweets:
-    print(tweet['content'])
+    img_url = tweet['media'][0]['previewUrl']
+    req = requests.get(img_url, allow_redirects=True)
+    with open(f'{img_dir}/{tweet["id"]}.jpg', 'wb') as f:
+        f.write(req.content)
 
 # Update last scrapped day
 with open(last_day_path, 'w') as f:
