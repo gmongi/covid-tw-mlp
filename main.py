99c1a796b8a6de66edd9280116eb841f8b239890
gmongi <guillox182@gmail.com>
2021-04-03 12:58:40 -0300

    Don't crop image and use Regex to find totalCases
    The position that we need varies for different dates
    Take the full image and change the parameters given to tesseract

--- main.py
+++ main.py
@@ -3,17 +3,12 @@ import csv
 import io
 import json
 import os
+import re
 from datetime import date
 
 import pytesseract
 import requests
 from PIL import Image
-
-# Constants
-LEFT_RATIO = .342
-TOP_RATIO = .605
-RIGHT_RATIO = .480
-BOTTOM_RATIO = .703
 
 # Config setup
 config = configparser.ConfigParser()
@@ -62,43 +57,43 @@ with open(tw_file_name) as f:
 # Work with the tweets
 # Download images to img_dir
 for tweet in json_tweets:
-    img_url = tweet['media'][0]['previewUrl']
+    img_url = tweet['media'][0]['fullUrl']
     req = requests.get(img_url, allow_redirects=True)
     img_bytes = io.BytesIO(req.content)
     with Image.open(img_bytes) as img:
-        # Crop total number of cases
-        cropped_img = img.crop((img.width * LEFT_RATIO,
-                                img.height * TOP_RATIO,
-                                img.width * RIGHT_RATIO,
-                                img.height * BOTTOM_RATIO))
-
         # Swap colors
-        clr_thld = 60
+        clr_thld = 100
         red = 223
         green = 10
         blue = 128
 
         # Pixel-by-pixel will do for now
-        width, height = cropped_img.size
+        width, height = img.size
         for x in range(0, width):
             for y in range(0, height):
-                pixel = cropped_img.getpixel((x, y))
+                pixel = img.getpixel((x, y))
                 if ((pixel[0] in range(red-clr_thld, red+clr_thld))
                    & (pixel[1] in range(green-clr_thld, green+clr_thld))
                    & (pixel[2] in range(blue-clr_thld, blue+clr_thld))):
-                    cropped_img.putpixel((x, y), (0, 0, 0))
+                    img.putpixel((x, y), (0, 0, 0))
 
         # Save
-        cropped_img.save(f'{img_dir}/{tweet["date"][0:10]}.jpg')
+        img.save(f'{img_dir}/{tweet["date"][0:10]}.jpg')
 
 # OCR all collected images
 with open('accumulatedTotalCases.csv', 'w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(['Date', 'CasesToDate'])
+    pattern = re.compile(r'.*CONFIRMADOS TOTALES\s*(\d+\.*\d*)')
     for img_file in os.listdir(img_dir):
         img = Image.open(f'{img_dir}/{img_file}')
-        ocr_str = pytesseract.image_to_string(img)
-        csv_writer.writerow([img_file[0:10], ocr_str.strip()])
+        ocr_str = pytesseract.image_to_string(img, config='--psm 6')
+        search = pattern.search(ocr_str)
+        if (search):
+            total_cases = search.group(1)
+        else:
+            total_cases = 'Error'
+        csv_writer.writerow([img_file[0:10], total_cases])
 
 # Update last scrapped day
 with open(last_day_path, 'w') as f:
