ca8022818e0e38e7b45454978b7c4aceaf62b02c
Adán Mauri Ungaro <adan.mauri@gmail.com>
2021-04-07 13:28:36 -0300

    Improved accuracy

--- main.py
+++ main.py
@@ -11,17 +11,21 @@ from utils import get_image_path, load_tweets_file, save_tweets_images
 
 load_dotenv()
 
-ERROR = os.getenv('ERROR')
-TW_PATH = os.getenv('TW_PATH')
-TW_ACCOUNT = os.getenv('TW_ACCOUNT')
-IMG_PATH = os.getenv('TW_PATH')
-MAX_RESULTS = os.getenv('MAX_RESULTS')
+IMG_ERROR = os.getenv('IMG_ERROR')
+IMG_PATH = os.getenv('IMG_PATH')
 LAST_DAY_FILENAME = os.getenv('LAST_DAY_FILENAME')
+MAX_RESULTS = os.getenv('MAX_RESULTS')
+OCR_ERROR = os.getenv('OCR_ERROR')
+TW_ACCOUNT = os.getenv('TW_ACCOUNT')
+TW_PATH = os.getenv('TW_PATH')
 try:
     file = open(LAST_DAY_FILENAME, 'r')
     DATE_SINCE = file.read()
 except IOError:
     DATE_SINCE = os.getenv('DATE_SINCE')
+WIDTH_VALUES = [350, 140]
+HEIGHT_VALUES = [180, 50]
+MARGIN_RESIZE_VALUES = [6, 6]
 
 # Images directory
 os.makedirs(IMG_PATH, exist_ok=True)
@@ -48,21 +52,19 @@ def main():
     # Call snscrape via OS library
     os.system(command)
 
-    json_tweets = load_tweets_file(TW_FILENAME)
-    save_tweets_images(IMG_PATH, json_tweets)
+    #json_tweets = load_tweets_file(TW_FILENAME)
+    #save_tweets_images(IMG_PATH, json_tweets)
 
     # OCR all collected images
     with open('accumulatedTotalCases.csv', 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(['Date', 'CasesToDate'])
-        pattern = re.compile(r'.*(\d+\.*\d*)')
         for img_file in os.listdir(IMG_PATH):
             img = cv2.imread(get_image_path(IMG_PATH, img_file))
             if img is None:
                 raise Exception(f'Image: {get_image_path(IMG_PATH, img_file)} does not exists')                
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             img = cv2.bitwise_not(img)
-
             _, thrash = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
             contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
             img_cropped = None
@@ -73,15 +75,23 @@ def main():
                 y = approx.ravel()[1] - 5
                 if len(approx) == 4:
                     x ,y, w, h = cv2.boundingRect(approx)
-                    aspectRatio = float(w)/h
-                    if not(aspectRatio >= 0.95 and aspectRatio <= 1.05) and w < 290 and w > 140 and h > 50 and h < 180:
-                        img_cropped = img[y:y+h, x:x+w]
+                    aspect_ratio = float(w)/h
+                    if (not(aspect_ratio >= 0.95 and aspect_ratio <= 1.05) and
+                        w < WIDTH_VALUES[0] and w > WIDTH_VALUES[1] and
+                        h < HEIGHT_VALUES[0] and h > HEIGHT_VALUES[1]):
+                        new_y = y+MARGIN_RESIZE_VALUES[0]
+                        new_x = x+MARGIN_RESIZE_VALUES[0]
+                        new_h = y+h-MARGIN_RESIZE_VALUES[1]
+                        new_w = x+w-MARGIN_RESIZE_VALUES[1]
+                        img_cropped = img[new_y:new_h, new_x:new_w]
                         break
             try:
-                ocr_str = pytesseract.image_to_string(img_cropped, config='--psm 7')
-                total_cases = int(ocr_str.strip().replace('.', '').replace('*', '')) if ocr_str else ERROR
-            except Exception:
-                total_cases = ERROR
+                ocr_str = pytesseract.image_to_string(img_cropped, config='--psm 13')
+                total_cases = ''.join(re.findall(r'\d+', ocr_str))
+            except TypeError:
+                total_cases = IMG_ERROR
+            except ValueError:
+                total_cases = OCR_ERROR
             csv_writer.writerow([img_file[0:10], total_cases])
 
 
