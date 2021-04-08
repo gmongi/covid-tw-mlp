462b7e3ecdcc128edfba463234606107a43ecd0c
Adán Mauri Ungaro <adan.mauri@gmail.com>
2021-04-08 00:25:32 -0300

    Improvements on Image Recognition, Readme and Linting + Small refactoring
    * Minor Readme and requirements.txt fixes. Image total cases recognition.
    
    * Refactor features to functions in utils.py file
    
    * Improved accuracy
    
    * Sorted results. Write in file once
    
    * Linter fixes + README Usage section
    
    Co-authored-by: gmongi <guillox182@gmail.com>

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
+## Prerequisites:
+* python 3.8
+* tesseract**: https://tesseract-ocr.github.io/tessdoc/Home.html#binaries
+
 ## Requirements
+* snscrape
+* Python Tesseract
+* OpenCV
 
+## Usage
+
+Create a .env file with according to .env.template
+
+Run with:
 ```bash
-pip install git+https://github.com/JustAnotherArchivist/snscrape.git
-```
-
-https://tesseract-ocr.github.io/tessdoc/Home.html#binaries
-
-```bash
-pip install pytesseract
-```
-
-```bash
-pip install opencv-python
+python main.py
 ```
--- main.py
+++ main.py
@@ -1,95 +1,116 @@
-import configparser
+'''
+main.py
+'''
+import argparse
 import csv
-import io
-import json
 import os
 import re
 
 import cv2
-import numpy as np
 import pytesseract
-import requests
-from PIL import Image
+from dotenv import load_dotenv
 
-# Config setup
-config = configparser.ConfigParser()
-config.read('config.ini')
-tw_dir = config['DEFAULT']['ScrappedTweeetsDir']
-tw_account = config['DEFAULT']['TwitterAccount']
-img_dir = config['DEFAULT']['ImageDownloadDir']
-max_results = config['DEFAULT']['MaxResults']
-last_day_path = 'lastScrapped.tmp'
-if os.path.exists(last_day_path):
-    with open(last_day_path, 'r') as f:
-        date_since = f.read()
-else:
-    date_since = config['DEFAULT']['DateSince']
+from utils import get_image_path, load_tweets_file, save_tweets_images
+
+load_dotenv()
+
+IMG_ERROR = os.getenv('IMG_ERROR')
+IMG_PATH = os.getenv('IMG_PATH')
+LAST_DAY_FILENAME = os.getenv('LAST_DAY_FILENAME')
+MAX_RESULTS = os.getenv('MAX_RESULTS')
+OCR_ERROR = os.getenv('OCR_ERROR')
+RESULTS_FILE = os.getenv('RESULTS_FILE')
+TW_ACCOUNT = os.getenv('TW_ACCOUNT')
+TW_PATH = os.getenv('TW_PATH')
+try:
+    file = open(LAST_DAY_FILENAME, 'r')
+    DATE_SINCE = file.read()
+except IOError:
+    DATE_SINCE = os.getenv('DATE_SINCE')
+WIDTH_VALUES = [350, 140]
+HEIGHT_VALUES = [180, 50]
+MARGIN_RESIZE_VALUES = [6, 6]
 
 # Images directory
-os.makedirs(img_dir, exist_ok=True)
+os.makedirs(IMG_PATH, exist_ok=True)
 
 # Tweets directory and json file
-os.makedirs(tw_dir, exist_ok=True)
-tw_file_name = f"{tw_dir}/{tw_account}-tweets.json"
+os.makedirs(TW_PATH, exist_ok=True)
+TW_FILENAME = f'{TW_PATH}/{TW_ACCOUNT}-tweets.json'
 
-# Command flags
-flags = {
-    '--jsonl': None,
-    '--progress': None,
-    '--max-results': max_results,
-    '--since': date_since,
-    'twitter-search': f'"(#Coronavirus) (from:{tw_account}) -filter:replies"',
-}
 
-# Build command string
-parsed_flags = [f"{key} {value or ''} " for key, value in flags.items()]
-command = f"snscrape {''.join(parsed_flags)} > {tw_file_name}"
+def main():
+    """ Main function """
 
-# Call snscrape via OS library
-os.system(command)
+    # Command flags
+    flags = {
+        '--jsonl': None,
+        '--progress': None,
+        '--max-results': MAX_RESULTS,
+        '--since': DATE_SINCE,
+        'twitter-search': f'"(#Coronavirus) (from:{TW_ACCOUNT}) -filter:replies"',
+    }
 
-# Read the json file we just scraped
-json_tweets = []
-with open(tw_file_name) as f:
-    for line in f:
-        tweet = json.loads(line)
-        json_tweets.append(tweet)
+    # Build command string
+    parsed_flags = [f"{key} {value or ''} " for key, value in flags.items()]
+    command = f"snscrape {''.join(parsed_flags)} > {TW_FILENAME}"
 
-# Work with the tweets
-# Download images to img_dir
-for tweet in json_tweets:
-    img_url = tweet['media'][0]['fullUrl']
-    req = requests.get(img_url, allow_redirects=True)
-    img_bytes = io.BytesIO(req.content)
-    with Image.open(img_bytes) as img:
-        # Swap colors
-        clr_thld = 100
-        red = 223
-        green = 10
-        blue = 128
+    # Call snscrape via OS library
+    os.system(command)
 
-        # Save
-        img.save(f'{img_dir}/{tweet["date"][0:10]}.jpg')
+    json_tweets = load_tweets_file(TW_FILENAME)
+    save_tweets_images(IMG_PATH, json_tweets)
 
-# OCR all collected images
-with open('accumulatedTotalCases.csv', 'w', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['Date', 'CasesToDate'])
-    pattern = re.compile(r'.*CONFIRMADOS TOTALES\s*(\d+\.*\d*)')
-    for img_file in os.listdir(img_dir):
-        img = cv2.imread(f'{img_dir}/{img_file}')
-        img = cv2.resize(img, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+    results = []
+    # OCR all collected images
+    for img_file in os.listdir(IMG_PATH):
+        date = img_file[0:10]
+        img = cv2.imread(get_image_path(IMG_PATH, img_file))
+        if img is None:
+            raise Exception(f'Image: {get_image_path(IMG_PATH, img_file)} does not exists')
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        kernel = np.ones((2, 2), np.uint8)
-        img = cv2.dilate(img, kernel, iterations=1)
-        img = cv2.erode(img, kernel, iterations=1)
-        img = cv2.threshold(cv2.bilateralFilter(img, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-        cv2.imwrite(f'{img_dir}/{img_file}', img)
+        img = cv2.bitwise_not(img)
+        _, thrash = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        img_cropped = None
+        for contour in contours:
+            approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
+            cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
+            x = approx.ravel()[0]
+            y = approx.ravel()[1] - 5
+            if len(approx) == 4:
+                x, y, w, h = cv2.boundingRect(approx)
+                aspect_ratio = float(w)/h
+                if (not(0.95 <= aspect_ratio <= 1.05) and
+                   WIDTH_VALUES[1] < w < WIDTH_VALUES[0] and
+                   HEIGHT_VALUES[1] < h < HEIGHT_VALUES[0]):
+                    new_y = y+MARGIN_RESIZE_VALUES[0]
+                    new_x = x+MARGIN_RESIZE_VALUES[0]
+                    new_h = y+h-MARGIN_RESIZE_VALUES[1]
+                    new_w = x+w-MARGIN_RESIZE_VALUES[1]
+                    img_cropped = img[new_y:new_h, new_x:new_w]
+                    break
+        try:
+            ocr_str = pytesseract.image_to_string(img_cropped, config='--psm 13')
+            total_cases = ''.join(re.findall(r'\d+', ocr_str))
+        except TypeError:
+            total_cases = IMG_ERROR
+        except ValueError:
+            total_cases = OCR_ERROR
+        results.append({'date': date, 'total_cases': total_cases})
+    results = sorted(results, key=lambda res: res['date'])
 
-        ocr_str = pytesseract.image_to_string(img, config='--psm 6')
-        search = pattern.search(ocr_str)
-        if (search):
-            total_cases = search.group(1)
-        else:
-            total_cases = 'Error'
-        csv_writer.writerow([img_file[0:10], total_cases])
+    with open(RESULTS_FILE, 'w', newline='') as csv_file:
+        keys = results[0].keys()
+        csv_writer = csv.DictWriter(csv_file, fieldnames=keys)
+        csv_writer.writeheader()
+        csv_writer.writerows(results)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Covid Twitter Municipalidad de La Plata')
+    parser.add_argument('--get-tweets', help='get tweets')
+    parser.add_argument('--load-images', help='load images')
+    parser.add_argument('--process-images', help='process images')
+    args = parser.parse_args()
+    main()
